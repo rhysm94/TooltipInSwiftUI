05d7be1d725cc9078d4582c38460a077e23af987
Rhys Morgan <rhysm@me.com>
2023-06-06 16:15:46 +0100

    Initial Commit

---
+++ TooltipInSwiftUI/TooltipInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooltipInSwiftUIApp.swift
+//  TooltipInSwiftUI
+//
+//  Created by Rhys Morgan on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TooltipInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
